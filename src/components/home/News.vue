3dd24c6c63605316586863124af42e9b56d54ea5
Hayato Kamiyama <kami8ma8810@gmail.com>
2023-04-17 15:46:36 +0900

    TOP お知らせタイトル変更

--- src/components/home/News.vue
+++ src/components/home/News.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="news-wrapper">
-    <h2 class="section-title">センターからのお知らせ</h2>
+    <h2 class="section-title">能センからのお知らせ</h2>
     <article
       class="news-article"
       v-for="(item, index) in $static.allContentfulNewsPost.edges"
-      :key="index"><span class="news-date">{{ item.node.date }}<br /></span><div class="news-content" v-html="richtextToHTML(item.node.article)" />
+      :key="index"
+    >
+      <span class="news-date">
+        {{ item.node.date }}
+        <br />
+      </span>
+      <div class="news-content" v-html="richtextToHTML(item.node.article)" />
     </article>
   </div>
 </template>
@@ -26,26 +32,28 @@ query News {
 </static-query>
 
 <script>
-import { INLINES } from '@contentful/rich-text-types';
-import { documentToHtmlString } from '../../../node_modules/@contentful/rich-text-html-renderer';
+import { INLINES } from "@contentful/rich-text-types";
+import { documentToHtmlString } from "../../../node_modules/@contentful/rich-text-html-renderer";
 const options = {
   //contentfulのエディタで設定したassetへのリンクを変換
   renderNode: {
-    [INLINES.ASSET_HYPERLINK]: (node) =>
-      `<a href="${node.data.target.fields.file.url}">${node.content[0].value}</a>`,
-  },
+    [INLINES.ASSET_HYPERLINK]: node =>
+      `<a href="${node.data.target.fields.file.url}">${
+        node.content[0].value
+      }</a>`
+  }
 };
 
 export default {
-  name: 'news',
+  name: "news",
   methods: {
     richtextToHTML(content) {
       const richtextString = documentToHtmlString(content, options)
         .replace(/\n/g, `</br>`)
         .replace(/<a((?: .+?))?>(.*?)<\/a>/g, '<a $1 target="_blank">$2</a>');
       return richtextString;
-    },
-  },
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
