5ca092bd15c621a82231ddcc1bb1961fd442d0f8
koderahayato <h.kodera0019@gmail.com>
2023-08-28 12:13:49 +0900

    0828修正

--- src/components/licensePage/NewPvMovie.vue
+++ src/components/licensePage/NewPvMovie.vue
@@ -55,7 +55,7 @@ export default {
     padding: 1rem;
     text-align: center;
     margin: 0;
-    box-shadow: 0px 5px 1px rgba(0, 107, 173, 0.5);
+    box-shadow: 0px 5px 1px rgba(0, 107, 173, 1);
     @media print, screen and (max-width: 560px) {
       font-size: 20px;
     }
