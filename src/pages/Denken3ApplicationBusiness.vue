c6d0d00e2c0adf03fa4b577f8670d72c13d30618
koderahayato <h.kodera0019@gmail.com>
2023-09-05 12:03:11 +0900

    0905修正

---
+++ src/pages/Denken3ApplicationBusiness.vue
@@ -0,0 +1,79 @@
+<template>
+  <Layout>
+    <div class="denken3-application">
+      <header class="header">
+        <Hero
+          title="第三種電気主任技術者（電験三種）<br>講座お申込み"
+          color="#1D9BF0"
+        />
+      </header>
+      <div class="contents">
+        <iframe
+          class="google_form"
+          src="https://business.form-mailer.jp/fms/6c81a8ac211740"
+          width="100%"
+          height="1000"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        >
+          読み込んでいます…
+        </iframe>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteUrl
+  }
+}
+</static-query>
+
+<script>
+import Hero from "@/components/Hero.vue";
+export default {
+  name: "denken3_application_business",
+  metaInfo() {
+    return {
+      titleTemplate:
+        "第三種電気主任技術者お申し込みフォーム（法人用）｜能セン-合格への近道-",
+      meta: [
+        {
+          name: "description",
+          content:
+            "能セン受験対策講習会「第三種電気主任技術者（電験三種）」の法人用お申し込みフォームです。通学講座、オンライン講座、通信講座共にこちらのフォームからお申込み頂けます。経験豊富な講師陣のポイントを絞った的確な講義をぜひ一度ご体験ください。"
+        },
+        {
+          key: `og:title`,
+          property: `og:title`,
+          // content: `第三種電気主任技術者お申し込みフォーム | ${
+          //   this.$static.metadata.siteName
+          // }`,
+          content: `第三種電気主任技術者お申し込みフォーム（法人用）｜能セン-合格への近道-`
+        },
+        {
+          key: `og:url`,
+          property: `og:url`,
+          content: `${this.$static.metadata.siteUrl}/denken3-application-business`
+        }
+      ]
+    };
+  },
+  components: {
+    Hero
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+// .denken3-application {
+// padding-bottom: 100px;
+// }
+.contents {
+  background-color: #f2f2f2;
+  padding-top: 50px;
+}
+</style>
