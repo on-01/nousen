53e24a6d490e3e28a17a575cb2b689662d478edd
koderahayato <h.kodera0019@gmail.com>
2024-01-11 17:14:16 +0900

    0111修正 -6

--- src/components/licensePage/Method.vue
+++ src/components/licensePage/Method.vue
@@ -23,7 +23,7 @@ export default {
     max-width: 1170px;
     width: 100%;
     background-color: #fff;
-    margin: 0 auto 20px;
+    margin: 0 auto;
     z-index: 0;
     position: relative;
     font-family: 'Meiryo', sans-serif;
