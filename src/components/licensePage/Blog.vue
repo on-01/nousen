f041e786977f8bbf1db5d459b73802597204260f
koderahayato <h.kodera0019@gmail.com>
2024-01-17 12:39:35 +0900

    0117修正

--- src/components/licensePage/Blog.vue
+++ src/components/licensePage/Blog.vue
@@ -5,9 +5,10 @@
             <p class="blog-line"></p>
             <div class="blog-links">
                 <a class="blog-link" href="https://denken.nouryoku.com/">● 電験三種【2024年】初心者必見！試験・合格率・難易度・仕事内容</a>
-                <a class="blog-link" href="https://nousen.blog/denken3/"><span class="blog-blank"></span>● 電験三種【令和6年度】試験日・難易度・合格率・勉強方法を徹底解説</a>
-                <a class="blog-link" href="https://nousen.blog/denken3-1/">● 電験三種【2024年】初心者必見！試験・合格率・難易度・仕事内容</a>
-                <a class="blog-link" href="https://nousen.blog/denken3-2/">● 電験三種【2024年】初心者必見！試験・合格率・難易度・仕事内容</a>
+                <a class="blog-link" href="https://nousen.blog/denken3/">● 電験三種【令和6年度】試験日・難易度・合格率・勉強方法を徹底解説</a>
+                <a class="blog-link" href="https://nousen.blog/denken3-1/">● 電験三種とは？試験内容・出題範囲・取得メリット公開【2024年】</a>
+                <a class="blog-link" href="https://nousen.blog/denken3-2/">● 電験三種の基礎数学・計算が苦手な方【初心者】数学の範囲・基礎講座</a>
+                <a class="blog-link" href="リンク:https://nousen.blog/3-4/">● よくわかる！電験三種（第三種電気主任技術者）の試験概要2024年</a>
             </div>
         </div>
     </div>
