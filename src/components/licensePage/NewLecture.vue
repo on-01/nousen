cf8c55346f93d241265f53c8ee5cab4355703886
koderahayato <h.kodera0019@gmail.com>
2024-04-05 18:11:51 +0900

    0405修正

--- src/components/licensePage/NewLecture.vue
+++ src/components/licensePage/NewLecture.vue
@@ -28,20 +28,20 @@
                     <p class="lecture-profile" v-html="profile"></p>
                 </div>
             </div>
-            
+
             <div class="add">
-                <p class="lecture-teacher">【基礎講座】</p>
-                <p class="lecture-profile">電気数学の基礎【四則計算(加減乗除)・比例・反比例・指数法則と対数関数・二次方程式の根・分数式・連立方程式・三角関数・ベクトルと正弦波交流】・科目学習のポイント</p>
-                <p class="lecture-teacher">【理論】</p>
-                <p class="lecture-profile">基礎数学・SI単位・電気・光・磁気効果・電気理論・電子理論・電気計測・電子計測・その他</p>
-                <p class="lecture-teacher">【電力】</p>
-                <p class="lecture-profile">発電所及び変電所の設計及び運転・送電線路及び配電線路(屋内配線を含む)の設計及び 運用・電気材料・その他</p>
-                <p class="lecture-teacher">【機械】</p>
-                <p class="lecture-profile">電気機器・パワーエレクトロニクス・電動機応用・照明・電熱・電気化学・電気加工・自動制御・メカトロニクス・電力システムに関する情報伝送及び処理・その他</p>
-                <p class="lecture-teacher">【法規】</p>
-                <p class="lecture-profile">電気法規(保安に関するものに限る)・電気施設管理・その他</p>
-                <p class="lecture-teacher">【模擬試験】</p>
-                <p class="lecture-profile">最新試験の直前予想模擬試験。模擬試験を取り組んだ後30分程度の解答解説講座</p>
+                <p class="add-text1">【基礎講座】</p>
+                <p class="add-text2">電気数学の基礎【四則計算(加減乗除)・比例・反比例・指数法則と対数関数・二次方程式の根・分数式・連立方程式・三角関数・ベクトルと正弦波交流】・科目学習のポイント</p>
+                <p class="add-text1">【理論】</p>
+                <p class="add-text2">基礎数学・SI単位・電気・光・磁気効果・電気理論・電子理論・電気計測・電子計測・その他</p>
+                <p class="add-text1">【電力】</p>
+                <p class="add-text2">発電所及び変電所の設計及び運転・送電線路及び配電線路(屋内配線を含む)の設計及び 運用・電気材料・その他</p>
+                <p class="add-text1">【機械】</p>
+                <p class="add-text2">電気機器・パワーエレクトロニクス・電動機応用・照明・電熱・電気化学・電気加工・自動制御・メカトロニクス・電力システムに関する情報伝送及び処理・その他</p>
+                <p class="add-text1">【法規】</p>
+                <p class="add-text2">電気法規(保安に関するものに限る)・電気施設管理・その他</p>
+                <p class="add-text1">【模擬試験】</p>
+                <p class="add-text2">最新試験の直前予想模擬試験。模擬試験を取り組んだ後30分程度の解答解説講座</p>
             </div>
         </div>
     </div>
@@ -138,6 +138,19 @@ export default {
     line-height: 1.6;
     margin: 0;
 }
+.add {
+    padding: 10px 30px;
+}
+.add-text1 {
+    font-size: 16px;
+    font-weight: 700;
+    margin: 0;
+}
+.add-text2 {
+    font-size: 14px;
+    line-height: 1.6;
+    margin: 0;
+}
 .kenchiku .add {
     display: none;
 }
@@ -167,6 +180,9 @@ export default {
 .lecture-text {
     font-size: 16px;
     line-height: 1.6;
+}
+.add {
+    padding: 0 20px;
 }
 
 }
