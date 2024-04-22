6ecfd08a80632f4e17cf57ef069f053a1cd23ed4
github m-omiya <m.omiya1127@gmail.com>
2024-04-22 13:34:48 +0900

    ADD:合格応援キャンペーンの記事の文言/font-size 変更

--- src/components/licensePage/CampaignDenken3.vue
+++ src/components/licensePage/CampaignDenken3.vue
@@ -1,174 +1,202 @@
 <template>
-    <div class="campaign">
-        <p class="campaign-title">合格応援キャンペーン(受講者特典)</p>
-        <div class="campaign-inner">
-            <div class="campaign-section">
-                <figure class="campaign-image">
-                    <img src="../../assets/img/license_page/denken3/denken3_campaign_1.jpg" alt="電験三種">
-                </figure>
-                <div class="campaign-description">
-                    <p class="campaign-topic -red">第二弾【￥5,000 off】早割キャンペーン（通学講座）<span style="color: black;">【終了】</span></p>
-                    <ul class="campaign-content">
-                        <li class="campaign-emphasis">・{{ date }}まで早割キャンペーン大好評につき延長！</li>
-                        <li>・通学講座の全科目セット・４科目セットのお申込みの方が対象になります</li>
-                        <li>早いスタートが合格への近道です！</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="campaign-section">
-                <figure class="campaign-image">
-                    <img src="../../assets/img/license_page/denken3/denken3_campaign_2.jpg" alt="電験三種">
-                </figure>
-                <div class="campaign-description">
-                    <p class="campaign-topic">電気主任技術者のバイブルを進呈</p>
-                    <ul class="campaign-content">
-                        <li>・電験男子・電験女子のバイブルと言えばオーム社さんの【新電気】。その最新号を進呈！</li>
-                        <li>・【基礎講座】または【全科目セット】受講の方が対象です（通学・オンライン・通信）</li>
-                        <li>※お申込み時期によっては前月号になる場合もあります</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="campaign-section">
-                <figure class="campaign-image">
-                    <img src="../../assets/img/license_page/denken3/denken3_campaign_3.jpg" alt="電験三種">
-                </figure>
-                <div class="campaign-description">
-                    <p class="campaign-topic">過去問題進呈</p>
-                    <ul class="campaign-content">
-                        <li>・過去の試験問題10年分を進呈します（通学・オンライン・通信講座）</li>
-                        <li>・メールアドレスが必要になります</li>
-                        <li>電験三種の試験突破には過去問題を多く解くことが非常に重要な役割を果たします。</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="campaign-section">
-                <figure class="campaign-image">
-                    <img src="../../assets/img/license_page/denken3/denken3_campaign_4.jpg" alt="電験三種">
-                </figure>
-                <div class="campaign-description">
-                    <p class="campaign-topic">出題分析を進呈</p>
-                    <ul class="campaign-content">
-                        <li>・能セン講座の合格率が高い理由の一つである【出題分析】。こちらを受講科目ごとに進呈！</li>
-                        <li>・過去の出題傾向を把握して対策を立てられます</li>
-                        <li>通学講座・オンライン講座では出題傾向による対策を毎年講義で行っております。</li>
-                    </ul>
-                </div>
-            </div>
+  <div class="campaign">
+    <p class="campaign-title">合格応援キャンペーン(受講者特典)</p>
+    <div class="campaign-inner">
+      <div class="campaign-section">
+        <figure class="campaign-image">
+          <img
+            src="../../assets/img/license_page/denken3/denken3_campaign_1.jpg"
+            alt="電験三種"
+          />
+        </figure>
+        <div class="campaign-description">
+          <p class="campaign-topic -red">
+            第二弾【￥5,000 off】早割キャンペーン（通学講座）
+            <span style="color: black">【終了】</span>
+          </p>
+          <ul class="campaign-content">
+            <li class="campaign-emphasis">
+              ・次回早割キャンペーンは７月頃予定になります！
+            </li>
+            <li>
+              ・通学講座の全科目セット・４科目セットのお申込みの方が対象になります
+            </li>
+            <li>早いスタートが合格への近道です！</li>
+          </ul>
         </div>
+      </div>
+      <div class="campaign-section">
+        <figure class="campaign-image">
+          <img
+            src="../../assets/img/license_page/denken3/denken3_campaign_2.jpg"
+            alt="電験三種"
+          />
+        </figure>
+        <div class="campaign-description">
+          <p class="campaign-topic">電気主任技術者のバイブルを進呈</p>
+          <ul class="campaign-content">
+            <li>
+              ・電験男子・電験女子のバイブルと言えばオーム社さんの【新電気】。その最新号を進呈！
+            </li>
+            <li>
+              ・【基礎講座】または【全科目セット】受講の方が対象です（通学・オンライン・通信）
+            </li>
+            <li>※お申込み時期によっては前月号になる場合もあります</li>
+          </ul>
+        </div>
+      </div>
+      <div class="campaign-section">
+        <figure class="campaign-image">
+          <img
+            src="../../assets/img/license_page/denken3/denken3_campaign_3.jpg"
+            alt="電験三種"
+          />
+        </figure>
+        <div class="campaign-description">
+          <p class="campaign-topic">過去問題進呈</p>
+          <ul class="campaign-content">
+            <li>
+              ・過去の試験問題10年分を進呈します（通学・オンライン・通信講座）
+            </li>
+            <li>・メールアドレスが必要になります</li>
+            <li>
+              電験三種の試験突破には過去問題を多く解くことが非常に重要な役割を果たします。
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="campaign-section">
+        <figure class="campaign-image">
+          <img
+            src="../../assets/img/license_page/denken3/denken3_campaign_4.jpg"
+            alt="電験三種"
+          />
+        </figure>
+        <div class="campaign-description">
+          <p class="campaign-topic">出題分析を進呈</p>
+          <ul class="campaign-content">
+            <li>
+              ・能セン講座の合格率が高い理由の一つである【出題分析】。こちらを受講科目ごとに進呈！
+            </li>
+            <li>・過去の出題傾向を把握して対策を立てられます</li>
+            <li>
+              通学講座・オンライン講座では出題傾向による対策を毎年講義で行っております。
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
+  export default {
     props: {
-        date: String,
-    } 
-}
+      date: String,
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.campaign {
+  .campaign {
     width: 100%;
     background-color: #fff;
     margin: 0px auto 40px;
     z-index: 0;
     position: relative;
-    font-family: 'Meiryo', sans-serif;
-}
-.campaign-inner {
+    font-family: "Meiryo", sans-serif;
+  }
+  .campaign-inner {
     margin: 0 auto;
     max-width: 1170px;
     padding: 20px;
-}
+  }
 
-.campaign-title {
-line-height: 1.3;
-font-size: 28px;
-font-weight: 700;
-color: #fff;
-background-color: #ed6c00;
-padding: 1rem;
-text-align: center;
-margin: 0;
-box-shadow: 0px 5px 1px rgba(161, 73, 0, 1);
+  .campaign-title {
+    line-height: 1.3;
+    font-size: 28px;
+    font-weight: 700;
+    color: #fff;
+    background-color: #ed6c00;
+    padding: 1rem;
+    text-align: center;
+    margin: 0;
+    box-shadow: 0px 5px 1px rgba(161, 73, 0, 1);
     @media print, screen and (max-width: 560px) {
-        font-size: 20px;
+      font-size: 20px;
     }
-}
-.campaign-section {
+  }
+  .campaign-section {
     display: flex;
     justify-content: space-between;
     margin-top: 20px;
-}
+  }
 
-.campaign-image {
+  .campaign-image {
     width: 25%;
-
-}
-.campaign-description {
+  }
+  .campaign-description {
     width: 70%;
     line-height: 2;
-}
-.campaign-topic {
+  }
+  .campaign-topic {
     font-weight: bold;
     font-size: 20px;
     color: #005a9e;
     margin: 10px 0 0;
-}
-.campaign-topic.-red {
+  }
+  .campaign-topic.-red {
     color: #ff0000;
-}
-.campaign-summary {
+  }
+  .campaign-summary {
     font-weight: medium;
     font-size: 16px;
     color: #005a9e;
     margin: 0;
-}
+  }
 
-.campaign-content {
+  .campaign-content {
     font-weight: normal;
-    font-size: 15px;
-}
-.campaign-content li span{
-    color: #ff0000
-}
+    font-size: 16px;
+  }
+  .campaign-content li span {
+    color: #ff0000;
+  }
 
-.campaign-emphasis {
+  .campaign-emphasis {
     color: #005a9e;
-}
+  }
 
-
-@media print, screen and (max-width: 1000px) {
+  @media print, screen and (max-width: 1000px) {
     .campaign-section {
-    width: 90%;
-    flex-flow: wrap;
-    margin: 0 auto;
+      width: 90%;
+      flex-flow: wrap;
+      margin: 0 auto;
     }
     .campaign-image {
-    width: 90%;
-    margin: 0 auto;
+      width: 90%;
+      margin: 0 auto;
     }
 
     .campaign-description {
-    width: 100%;
-    margin-top: 20px;
+      width: 100%;
+      margin-top: 20px;
     }
-}
+  }
 
-@media print, screen and (max-width: 560px) {
+  @media print, screen and (max-width: 560px) {
     .campaign-description {
-    margin-top: 10px;
-    line-height: 1.8;
+      margin-top: 10px;
+      line-height: 1.8;
     }
     .campaign-topic {
-    font-size: 18px;
+      font-size: 18px;
     }
     .campaign-summary {
-        font-size: 14px;
+      font-size: 14px;
     }
     .campaign-content {
-        font-size: 14px;
+      font-size: 14px;
     }
-}
-
+  }
 </style>
