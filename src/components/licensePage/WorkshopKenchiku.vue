311ea79dfabcbe844141e87d4d9f83b7dc3d2290
github m-omiya <m.omiya1127@gmail.com>
2024-07-05 10:27:55 +0900

    ADD:建築２級建築施工管理技士 通学講座（講習会）追加

---
+++ src/components/licensePage/WorkshopKenchiku.vue
@@ -0,0 +1,285 @@
+<template>
+  <div class="workshop">
+    <p class="workshop-title">２級建築施工管理技士 通学講座（講習会）</p>
+    <p class="workshop-top">
+      お仕事等で忙しく何日も通う事が困難な方でも、短期間で効率よく学べる通学講習会になっています。
+      <br />
+      経験豊富な講師陣のポイントを絞った的確な講義を是非ご体験ください！
+    </p>
+    <div class="workshop-wrapper">
+      <div class="workshop-box">
+        <p class="workshop-heading">前期</p>
+        <div class="workshop-content">
+          <p class="workshop-text">一次コース（２日間）</p>
+          <p class="workshop-text">【終了】</p>
+          <p class="workshop-text">
+            共通科目（建築学・施工・施工管理法）/ 選択科目（☆新設
+            応用能力問題・法規）
+          </p>
+        </div>
+        <p class="workshop-heading -bottom">後期（定員３０名）</p>
+        <div class="workshop-content">
+          <p class="workshop-text">一次コース（２日間）</p>
+          <p class="workshop-text">
+            １０月１２日（土）～１３日（日）９：３０～１７：００
+            <span>【受付中】</span>
+          </p>
+          <p class="workshop-text">
+            共通科目（建築学・施工・施工管理法）/ 選択科目（☆新設
+            応用能力問題・法規）
+          </p>
+        </div>
+        <div class="workshop-content">
+          <p class="workshop-text">二次コース（１日間）</p>
+          <p class="workshop-text">
+            １０月１９日（土）１０：００～１６：００
+            <span>【受付中】</span>
+          </p>
+          <p class="workshop-text">
+            施工経験記述 / 施工用語 / 工程管理 / 法規 / 施工技術 / その他
+          </p>
+        </div>
+      </div>
+    </div>
+    <p class="workshop-bottom -yellow">
+      <span>当日欠席しても安心！オンライン講座に無料振替OK！</span>
+    </p>
+    <p class="workshop-bottom">
+      ※仕事や体調不良などで欠席した場合はオンライン講座に無料振替ができます
+    </p>
+
+    <p class="workshop-title -bottom">
+      ２級建築施工管理技士 オンライン講座（WEB講座）
+    </p>
+    <p class="workshop-top">
+      能センがおすすめするオンライン講座（WEB講座）は、短期間での合格に特化した受験対策講座。
+      <br />
+      高性能な動画配信プラットフォームを使い、最新動画をPCやスマホから何回でも視聴できます！
+    </p>
+    <div class="workshop-wrapper">
+      <div class="workshop-box">
+        <p class="workshop-heading">前期</p>
+        <div class="workshop-content">
+          <p class="workshop-text">一次コース（約１２時間）</p>
+          <p class="workshop-text">【終了】</p>
+          <p class="workshop-text">
+            共通科目（建築学・施工・施工管理法）/ 選択科目（☆新設
+            応用能力問題・法規）
+          </p>
+        </div>
+        <p class="workshop-heading -bottom">後期</p>
+        <div class="workshop-content">
+          <p class="workshop-text">一次コース（約１２時間）</p>
+          <p class="workshop-text">
+            令和６年１０月１７日(木) ～１１月３０日(土)まで
+            <span>【受付中】</span>
+          </p>
+          <p class="workshop-text">
+            共通科目（建築学・施工・施工管理法）/ 選択科目（☆新設
+            応用能力問題・法規）
+          </p>
+        </div>
+        <div class="workshop-content">
+          <p class="workshop-text">二次コース（約５時間）</p>
+          <p class="workshop-text">
+            令和６年１０月２３日(水) ～１１月３０日(土)まで
+            <span>【受付中】</span>
+          </p>
+          <p class="workshop-text">
+            施工経験記述 / 施工用語 / 工程管理 / 法規 / 施工技術 / その他
+          </p>
+        </div>
+        <p class="workshop-heading -blue -bottom">
+          昨年度の動画（無料サービス）
+        </p>
+        <div class="workshop-content">
+          <p class="workshop-text -blue">
+            一次コース（約１２時間） / 二次コース（約５時間）
+          </p>
+          <p class="workshop-text -blue">
+            令和６年６月1日(土) ～ １１月３０日(土)まで
+          </p>
+        </div>
+      </div>
+    </div>
+    <p class="workshop-bottom -yellow">
+      <span>配信終了日まで何回でも視聴可能です！</span>
+    </p>
+    <p class="workshop-bottom">※昨年度の動画は無料で配信いたします</p>
+
+    <div class="add">
+      <p class="add-title">学習内容</p>
+      <div class="add-wrapper">
+        <p class="add-text1">【一次コース】</p>
+        <p class="add-text2">
+          ✅共通科目（建築学・施工・施工管理法）/ 選択科目（☆新設
+          応用能力問題・法規）
+          <br />
+          ▶試験に出るポイントを絞った講座内容
+          <br />
+          問題演習が多く、用語解説、ポイント解説、実際の現場での経験談など。
+          <br />
+          １７歳以上の学生や、二次試験の受験資格に満たない方にもおすすめ！
+        </p>
+        <br />
+        <p class="add-text1">【二次コース】</p>
+        <p class="add-text2">
+          ✅施工経験記述 / 施工用語 / 工程管理 / 法規 / 施工技術 / その他
+          <br />
+          ▶ 二次試験対策＋経験記述の添削付き
+          <br />
+          経験記述問題の対策が中心になります。経験記述試験はどのような問題が出題されるのか、書き方から
+          ポイント及び注意点までをわかり易く説明いたします。
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .workshop {
+    background-color: #fff;
+  }
+
+  .workshop-title {
+    line-height: 1.3;
+    font-size: 28px;
+    font-weight: 700;
+    color: #fff;
+    background-color: #1da1f2;
+    padding: 1rem;
+    text-align: center;
+    -webkit-box-shadow: 0 5px 1px #006bad;
+    box-shadow: 0 5px 1px #006bad;
+    margin-top: 0;
+    @media print, screen and (max-width: 768px) {
+      font-size: 20px;
+    }
+  }
+
+  .workshop-title.-bottom {
+    margin-top: 30px;
+  }
+
+  .workshop-top {
+    margin-top: 30px;
+    text-align: center;
+  }
+
+  .workshop-wrapper {
+    padding: 10px 30px;
+    margin: 0 auto;
+    overflow: scroll;
+    max-width: 1440px;
+  }
+
+  .workshop-box {
+    max-width: 1200px;
+    min-width: 1200px;
+    margin: 0 auto;
+  }
+
+  .workshop-heading {
+    text-align: center;
+    background-color: #ed6c00;
+    color: #ffffff;
+    font-size: 18px;
+    padding: 10px 0;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  .workshop-heading.-blue {
+    background-color: #2a78fe;
+  }
+
+  .workshop-heading.-bottom {
+    margin-top: 30px;
+  }
+
+  .workshop-content {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .workshop-text {
+    width: calc(100% / 3);
+    border: solid 1px;
+    border-top: none;
+    text-align: center;
+    padding: 10px 0;
+    font-feature-settings: "palt";
+    margin-top: 0;
+    margin-bottom: 0;
+    span {
+      color: red;
+    }
+  }
+
+  .workshop-text.-blue {
+    width: calc(100% / 2);
+    span {
+      color: red;
+    }
+  }
+
+  .workshop-text:nth-of-type(2),
+  .workshop-text:nth-of-type(3) {
+    border-left: none;
+    @media print, screen and (max-width: 1024px) {
+      border-left: solid 1px;
+    }
+  }
+
+  .workshop-bottom {
+    text-align: center;
+    margin-top: 0;
+    margin-bottom: 0;
+    line-height: 1.5;
+  }
+
+  .workshop-bottom.-yellow {
+    text-align: center;
+    span {
+      background: #ffff8d;
+      padding: 1px;
+    }
+  }
+
+  .add {
+    padding: 10px 30px 30px;
+    margin: 0 auto;
+    max-width: 1170px;
+  }
+  .add-wrapper {
+    max-width: 755px;
+    margin: 0 auto;
+  }
+  .add-text1 {
+    font-size: 18px;
+    font-weight: 700;
+    margin: 0;
+  }
+  .add-text2 {
+    font-size: 16px;
+    line-height: 1.6;
+    margin: 0;
+  }
+  .add-title {
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 1;
+    text-align: center;
+    color: #000;
+    border: 2px solid #1da1f2;
+    padding: 8px 0;
+    margin: 36px 0 12px;
+  }
+
+  @media print, screen and (max-width: 1000px) {
+    .add {
+      padding: 0 20px;
+    }
+  }
+</style>
