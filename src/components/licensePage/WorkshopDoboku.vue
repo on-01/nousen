de8701ee99b6ce6deebf489a15a439dbd18a6457
github m-omiya <m.omiya1127@gmail.com>
2024-09-20 10:06:08 +0900

    ADD：文字色変更

--- src/components/licensePage/WorkshopDoboku.vue
+++ src/components/licensePage/WorkshopDoboku.vue
@@ -34,13 +34,9 @@
           </div>
           <div class="workshop-text -third">
             <p class="workshop-border">
-              8月31日(土)～9月1日(日)9:30～17:00
-              <span>【終了】</span>
+              8月31日(土)～9月1日(日)9:30～17:00【終了】
             </p>
-            <p class="workshop-border -none">
-              9月15日(日)10:00～16:00
-              <span>【終了】</span>
-            </p>
+            <p class="workshop-border -none">9月15日(日)10:00～16:00【終了】</p>
           </div>
           <!-- <p class="workshop-text">
             土木一般 / 専門土木 / 法規 / 共通工学 / 施工管理法
