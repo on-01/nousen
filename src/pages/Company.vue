153bc3dd5bdbb1596939a606f450c226f1bb76a4
github m-omiya <m.omiya1127@gmail.com>
2024-10-23 14:48:02 +0900

    ADD:会社概要に追加

--- src/pages/Company.vue
+++ src/pages/Company.vue
@@ -116,6 +116,188 @@
           </div>
         </section>
 
+        <section class="section03">
+          <h2 class="section-title">
+            <img
+              src="../assets/img/front/reason_main_headline3.svg"
+              alt="能センの社会貢献活動"
+            />
+          </h2>
+          <div class="last-message">
+            <div class="last-message-flex">
+              <div class="last-message-flex-box">
+                <p class="last-message-flexTitle">
+                  こどもの未来応援基金への寄付
+                </p>
+                <img
+                  src="../assets/img/common/social_image1.jpg"
+                  alt="「こどもの未来応援基金への寄付」"
+                />
+                <p class="last-message-flexText-bold">
+                  日本を元気に！
+                  <br />
+                  子どもたちが夢と希望を持って働ける世の中へ。
+                </p>
+                <p class="last-message-flexText">
+                  能センでは、
+                  <span>受講生1名につき100円</span>
+                  を「こどもの未来応援基金」へ寄付いたします。
+                  <br />
+                  「こどもの未来応援基金」は、子どもの貧困対策を進めるための
+                  <br />
+                  官民連携プロジェクトの一環として、2015年に創設されました。
+                </p>
+                <p class="last-message-flexText">
+                  こどもの未来応援基金についての詳細は、以下のリンクをご参照ください。
+                  <br />
+                  [
+                  <a href="https://kodomohinkon.go.jp/">
+                    https://kodomohinkon.go.jp/
+                  </a>
+                  ]
+                </p>
+              </div>
+              <div class="last-message-flex-box">
+                <p class="last-message-flexTitle">
+                  環境への配慮～持続可能なミライへ～
+                </p>
+                <img
+                  src="../assets/img/common/social_image2.jpg"
+                  alt="環境への配慮～持続可能なミライへ～"
+                />
+                <p class="last-message-flexText-bold">
+                  能センでは、すべてのプリントや資料に
+                  <br />
+                  FSC認証を受けたエコペーパーを使用しております。
+                </p>
+                <p class="last-message-flexText">
+                  FSC認証とは、持続可能な森林の活用・保全を目的とし、
+                  <br />
+                  「適切な森林管理」を認証する国際的な制度です。
+                </p>
+                <p class="last-message-flexText">
+                  FSC認証についての詳細は、以下のリンクをご参照ください。
+                  <br />
+                  [
+                  <a href="https://shitte-erabo.net/aboutfsc/certification/">
+                    https://shitte-erabo.net/aboutfsc/certification/
+                  </a>
+                  ]
+                </p>
+              </div>
+              <!-- <img
+              src="../assets/img/common/social_image.jpg"
+              alt="子供の未来は日本の未来"
+              class="last-message-logo"
+              width="174"
+              height="93"
+              decoding="async"
+            />
+            <div class="last-message-description">
+              <p class="last-message-text1">
+                能センは子供の未来を
+                <br />
+                応援します!
+              </p>
+              <p class="last-message-text2">
+                日本を元気に！
+                <br />
+                子供たちが夢と希望を持って働ける世の中へ！
+                <br />
+                能センでは受講生１名につき１００円を
+                <br />
+                「こどもの未来応援基金」へ寄付をいたします。
+              </p>
+              <p class="last-message-text3">
+                <g-link to="https://kodomohinkon.go.jp/">
+                  こどもの未来応援基金
+                </g-link>
+                は、
+                <br />
+                こどもの貧困対策を進めるための
+                <br />
+                官民連携プロジェクトのひとつとして、
+                <br />
+                ２０１５年に創設されました。
+                <br />
+              </p>
+            </div> -->
+            </div>
+            <div class="last-message-flex -bottom">
+              <div class="last-message-flex-box">
+                <p class="last-message-flexTitle">
+                  建設業の発展と新規企業の成長をサポート！
+                </p>
+                <img
+                  src="../assets/img/common/social_image3.jpg"
+                  alt="建設業の発展と新規企業の成長をサポート！"
+                />
+                <p class="last-message-flexText-bold">
+                  建設業界の発展と新規企業の成長をサポート!
+                  <br />
+                  ミライを創ろう【創業支援割引制度】
+                </p>
+                <p class="last-message-flexText">
+                  建設業界の発展と新規企業の成長を支援するため、
+                  <br />
+                  創業2年以内の個人事業主および法人企業様向けに
+                  <span class="red">受講料を20％割引</span>
+                  でご提供します。
+                </p>
+                <p class="last-message-flexText -center">
+                  <!-- <span>対象者:&nbsp;</span>
+                創業2年以内の個人事業主および法人企業の従業員様
+                <br />
+                <span>割引率:&nbsp;</span>
+                受講料20％割引
+                <br /> -->
+                  <span>必要事項:&nbsp;</span>
+                  特に提出書類は不要ですが、以下の情報が必要となります。
+                </p>
+                <p class="last-message-flexText">
+                  <span>・本社住所 ・代表電話番号　・代表者氏名</span>
+                </p>
+                <p class="last-message-flexText">
+                  能センは、このプログラムを通じて、建設業界の新しい担い手を応援し、
+                  <br />
+                  業界全体の持続的な発展に貢献したいと考えています。
+                  <br />
+                  皆様の挑戦を、私たちも全力でサポートさせていただきます。
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="btn__apply __bottom">
+            <a @click="openModal">
+              <span>お申込みはこちら</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="25"
+                height="25"
+                viewBox="0 0 18 18"
+                aria-labelledby="box"
+                role="presentation"
+                class="icon"
+                data-v-2d92a3c2=""
+                data-v-0aac86ab=""
+              >
+                <title id="box" lang="en" data-v-2d92a3c2="">box icon</title>
+                <g fill="currentColor" data-v-2d92a3c2="">
+                  <g data-v-0aac86ab="" data-v-2d92a3c2="">
+                    <path
+                      d="M.17,8A5.06,5.06,0,0,1,5.23,2.94a5.11,5.11,0,0,1,4,1.89h4.22a2.43,2.43,0,0,1,0,4.85H10A5.07,5.07,0,0,1,.17,8ZM8.94,6.44H8.85a.79.79,0,0,1-.73-.36,3.47,3.47,0,1,0,.52,2.6A.8.8,0,0,1,9.42,8h4a.8.8,0,1,0,0-1.6H8.94Z"
+                    ></path>
+                    <path
+                      d="M7.74,5.65V7.19a2.54,2.54,0,0,1-5.08,0V5.65a2.54,2.54,0,0,1,5.08-.23A.89.89,0,0,1,7.74,5.65ZM4.27,7.19a.94.94,0,0,0,1.87.14V5.65a.94.94,0,0,0-1.87-.14V7.19Z"
+                    ></path>
+                  </g>
+                </g>
+              </svg>
+            </a>
+          </div>
+        </section>
+
         <Fsc title="持続可能なミライへ" />
 
         <div class="last-message">
@@ -396,6 +578,305 @@ query {
   .last-message-description {
     width: 55%;
     background: #fff8cc;
+    padding-left: 8%;
+    position: relative;
+  }
+  .last-message-description::after {
+    content: "";
+    position: absolute;
+    left: -322px;
+    bottom: 0;
+    width: 0;
+    height: 0;
+    border-left: 548px solid transparent;
+    border-top: 96px solid #fff8cc;
+    transform: rotate(90deg);
+  }
+  .last-message-text1 {
+    color: #0071bb;
+    font-size: 28px;
+    font-weight: 700;
+    padding-top: 10px;
+    line-height: 1.8;
+    letter-spacing: 0.1em;
+  }
+  .last-message-text2 {
+    font-size: 15px;
+    font-weight: 700;
+    padding-top: 10px;
+    line-height: 1.6;
+    letter-spacing: 0.1em;
+  }
+  .last-message-text3 {
+    font-size: 14px;
+    font-weight: 500;
+    padding-top: 10px;
+    padding-bottom: 8px;
+    line-height: 1.6;
+    letter-spacing: 0.1em;
+  }
+  .last-message-text3 a {
+    text-decoration: underline;
+  }
+
+  .section03 {
+    position: relative;
+    margin-top: -225px;
+    // z-index: -1;
+    @media print, screen and (max-width: 1000px) {
+      margin-top: -55px;
+    }
+    &:before {
+      content: "";
+      // height: 500px;
+      height: 305px;
+      width: 5px;
+      background-color: #000;
+      display: block;
+      position: relative;
+      top: 5px;
+      left: 0;
+      right: 0;
+      margin: auto;
+      z-index: -2;
+      @media print, screen and (max-width: 1000px) {
+        // height: 300px;
+        height: 220px;
+      }
+    }
+    &:after {
+      content: "";
+      height: 30px;
+      width: 5px;
+      background-color: #000;
+      display: block;
+      position: absolute;
+      // top: 473px;
+      top: 283px;
+      left: -20px;
+      right: 0;
+      margin: auto;
+      transform: rotate(-45deg);
+      @media print, screen and (max-width: 1000px) {
+        // top: 274px;
+        top: 196px;
+      }
+    }
+    .section-title {
+      max-width: 330px;
+      width: 100%;
+      margin: 0 auto;
+      background-color: #fff;
+      position: absolute;
+      // top: 200px;
+      top: 0;
+      left: 0;
+      right: 0;
+      padding: 50px 0;
+      @media print, screen and (max-width: 1000px) {
+        width: 90%;
+        top: 0;
+        max-width: 250px;
+        padding: 20px 0;
+      }
+      &:before {
+        content: url("../assets/img/icon/finger_top_brown.svg");
+        width: 30px;
+        height: 50px;
+        display: block;
+        margin: 0 auto;
+        background-color: #fff;
+        padding: 10px;
+      }
+    }
+    .reason-wrapper {
+      margin-top: 100px;
+      .btn__apply {
+        width: 100%;
+        text-align: center;
+        margin: 100px auto 200px;
+        @media print, screen and (max-width: 1000px) {
+          margin: 100px auto 150px;
+        }
+
+        a {
+          // 非表示に
+          display: none;
+          // display: inline-block;
+          background-color: #f4891e;
+          color: #fff;
+          padding: 10px 3em;
+          font-weight: bold;
+          font-size: 20px;
+          width: 375px;
+          box-sizing: border-box;
+        }
+        svg {
+          margin-left: 10px;
+        }
+      }
+    }
+    .btn__apply.__bottom {
+      width: 100%;
+      text-align: center;
+      margin: 100px auto 0;
+      // 非表示に
+      display: none;
+
+      a {
+        display: inline-block;
+        background-color: #f4891e;
+        color: #fff;
+        padding: 10px 3em;
+        font-weight: bold;
+        font-size: 20px;
+        width: 375px;
+        box-sizing: border-box;
+      }
+      svg {
+        margin-left: 10px;
+      }
+    }
+    .last-message {
+      // max-width: 970px;
+      max-width: 1300px;
+      padding: 0 20px;
+      // width: 100%;
+      margin: 0 auto;
+      overflow: hidden;
+      font-family: "メイリオ", sans-serif;
+      // @media print, screen and (max-width: 1000px) {
+      //   width: 90%;
+      // }
+      .under_display {
+        @media print, screen and (min-width: 1001px) {
+          display: none;
+        }
+      }
+      // &:before {
+      //   content: "";
+      //   height: 200px;
+      //   width: 5px;
+      //   background-color: #000;
+      //   display: block;
+      //   position: relative;
+      //   left: 0;
+      //   right: 0;
+      //   margin: auto;
+      //   margin-top: 30px;
+      //   @media print, screen and (max-width: 1000px) {
+      //     height: 100px;
+      //   }
+      // }
+    }
+  }
+  .last-message-flex {
+    display: flex;
+    margin: 80px auto 0;
+    // align-items: center;
+    justify-content: space-between;
+    @media print, screen and (max-width: 1000px) {
+      flex-direction: column;
+      row-gap: 40px;
+    }
+
+    .last-message-flex-box {
+      width: 50%;
+      @media print, screen and (max-width: 1000px) {
+        width: 100%;
+      }
+      img {
+        width: 63%;
+        display: block;
+        margin: 20px auto 10px;
+      }
+    }
+    .last-message-flexTitle {
+      font-size: 24px;
+      padding: 0 40px;
+      text-align: center;
+      color: white;
+      font-weight: 700;
+      margin: 0 auto;
+      border: solid 2px #333333;
+      background-color: #00af50;
+      width: 75%;
+      @media print, screen and (max-width: 570px) {
+        font-size: 20px;
+        padding: 0 20px;
+        width: 85%;
+      }
+    }
+    .last-message-flexText-bold {
+      text-align: center;
+      font-size: 15px;
+      font-weight: 700;
+      margin: 0;
+      line-height: 1.5;
+    }
+    .last-message-flexText {
+      text-align: center;
+      font-size: 15px;
+      margin: 10px 0 0;
+      line-height: 1.5;
+
+      span {
+        font-weight: 700;
+      }
+    }
+  }
+
+  .last-message-flex.-bottom {
+    justify-content: center;
+    @media print, screen and (max-width: 1000px) {
+      margin-top: 40px;
+    }
+
+    .last-message-flex-box {
+      img {
+        width: 80%;
+        @media print, screen and (max-width: 1000px) {
+          width: 63%;
+        }
+      }
+    }
+
+    .last-message-flexText-bold {
+      font-size: 17px;
+      @media print, screen and (max-width: 1000px) {
+        font-size: 15px;
+      }
+    }
+
+    .last-message-flexTitle {
+      background-color: #ed6c00;
+      padding: 10px 90px;
+      @media print, screen and (max-width: 1000px) {
+        padding: 0 40px;
+      }
+      @media print, screen and (max-width: 570px) {
+        padding: 0 20px;
+      }
+    }
+    .red {
+      color: red;
+    }
+    .last-message-flexText.-center {
+      text-align: left;
+      margin: 10px auto 0;
+      max-width: 500px;
+    }
+  }
+
+  .last-message-flex p {
+    margin: 0;
+  }
+  .last-message-logo {
+    width: 45%;
+  }
+  .last-message-description {
+    width: 55%;
+    // background: #fff8cc;
     padding-left: 8%;
     position: relative;
   }
