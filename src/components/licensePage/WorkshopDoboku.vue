a99625ea2cbe1075555db613858edcf737061673
github m-omiya <m.omiya1127@gmail.com>
2024-11-05 21:54:39 +0900

    ADD:調整

--- src/components/licensePage/WorkshopDoboku.vue
+++ src/components/licensePage/WorkshopDoboku.vue
@@ -57,36 +57,35 @@
             / 施工計画
           </p>
         </div> -->
-      </div>
-    </div>
-    <div class="workshop-wrapper">
-      <div class="add-tableWrap">
-        <div class="workshop-content -ryoukin">
-          <p class="workshop-text -ryoukin -orange"></p>
-          <p class="workshop-text -ryoukin -orange">一次・二次セット</p>
-          <p class="workshop-text -ryoukin -orange">一次コース</p>
-          <p class="workshop-text -ryoukin -orange">二次コース</p>
-        </div>
-        <div class="workshop-content -ryoukin">
-          <p class="workshop-text -ryoukin -bold">受講料</p>
-          <p class="workshop-text -ryoukin">
-            ４１,０００円
-            <br />
-            (税込４５,１００円)
-          </p>
-          <p class="workshop-text -ryoukin">
-            ３３,０００円
-            <br />
-            (税込３６,３００円)
-          </p>
-          <p class="workshop-text -ryoukin">
-            １６,３００円
-            <br />
-            (税込１７,９３０円)
-          </p>
+        <div class="add-tableWrap">
+          <div class="workshop-content -ryoukin">
+            <p class="workshop-text -ryoukin -orange"></p>
+            <p class="workshop-text -ryoukin -orange">一次・二次セット</p>
+            <p class="workshop-text -ryoukin -orange">一次コース</p>
+            <p class="workshop-text -ryoukin -orange">二次コース</p>
+          </div>
+          <div class="workshop-content -ryoukin">
+            <p class="workshop-text -ryoukin -bold">受講料</p>
+            <p class="workshop-text -ryoukin">
+              ４１,０００円
+              <br />
+              (税込４５,１００円)
+            </p>
+            <p class="workshop-text -ryoukin">
+              ３３,０００円
+              <br />
+              (税込３６,３００円)
+            </p>
+            <p class="workshop-text -ryoukin">
+              １６,３００円
+              <br />
+              (税込１７,９３０円)
+            </p>
+          </div>
         </div>
       </div>
     </div>
+
     <p class="workshop-bottom -yellow">
       <span>当日欠席しても安心！オンライン講座に無料振替OK！</span>
     </p>
@@ -236,32 +235,30 @@
             / 施工計画
           </p>
         </div> -->
-        <div class="workshop-wrapper">
-          <div class="add-tableWrap">
-            <div class="workshop-content -ryoukin">
-              <p class="workshop-text -ryoukin -orange"></p>
-              <p class="workshop-text -ryoukin -orange">一次・二次セット</p>
-              <p class="workshop-text -ryoukin -orange">一次コース</p>
-              <p class="workshop-text -ryoukin -orange">二次コース</p>
-            </div>
-            <div class="workshop-content -ryoukin">
-              <p class="workshop-text -ryoukin -bold">受講料</p>
-              <p class="workshop-text -ryoukin">
-                ３９,０００円
-                <br />
-                (税込４２,９００円)
-              </p>
-              <p class="workshop-text -ryoukin">
-                ３０,０００円
-                <br />
-                (税込３３,０００円)
-              </p>
-              <p class="workshop-text -ryoukin">
-                １６,３００円
-                <br />
-                (税込１７,９３０円)
-              </p>
-            </div>
+        <div class="add-tableWrap">
+          <div class="workshop-content -ryoukin">
+            <p class="workshop-text -ryoukin -orange"></p>
+            <p class="workshop-text -ryoukin -orange">一次・二次セット</p>
+            <p class="workshop-text -ryoukin -orange">一次コース</p>
+            <p class="workshop-text -ryoukin -orange">二次コース</p>
+          </div>
+          <div class="workshop-content -ryoukin">
+            <p class="workshop-text -ryoukin -bold">受講料</p>
+            <p class="workshop-text -ryoukin">
+              ３９,０００円
+              <br />
+              (税込４２,９００円)
+            </p>
+            <p class="workshop-text -ryoukin">
+              ３０,０００円
+              <br />
+              (税込３３,０００円)
+            </p>
+            <p class="workshop-text -ryoukin">
+              １６,３００円
+              <br />
+              (税込１７,９３０円)
+            </p>
           </div>
         </div>
         <p class="workshop-heading -blue -bottom">昨年度の動画(無料サービス)</p>
@@ -812,7 +809,7 @@
   .add-tableWrap {
     max-width: 800px;
     min-width: 800px;
-    margin: 0 auto;
+    margin: 20px auto 0 auto;
     -ms-overflow-style: none; /* IE, Edge 対応 */
     scrollbar-width: none; /* Firefox 対応 */
     &::-webkit-scrollbar {
