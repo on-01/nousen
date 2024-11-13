33aa9fe9f0130bed407428542e59906fe02d65c1
github m-omiya <m.omiya1127@gmail.com>
2024-11-13 15:38:41 +0900

    ADD:３種バナー変更

--- src/pages/Denken3.vue
+++ src/pages/Denken3.vue
@@ -15,7 +15,7 @@
           />
           <Cheer
             :imagePath="
-              require('../assets/img/license_page/denken3/denken3_point3.jpg')
+              require('../assets/img/license_page/denken3/denken3_point4.jpg')
             "
             altText="電験三種 講座"
           />
