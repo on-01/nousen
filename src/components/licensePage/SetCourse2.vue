a4dc0ce56990ca2a287ca19ec5d4b1cefa0029cd
github m-omiya <m.omiya1127@gmail.com>
2025-01-20 15:21:31 +0900

    ADD:表の幅調整

--- src/components/licensePage/SetCourse2.vue
+++ src/components/licensePage/SetCourse2.vue
@@ -224,8 +224,8 @@
     scrollbar-width: none;
   }
   .setCourse-table {
-    max-width: 700px;
-    min-width: 700px;
+    max-width: 600px;
+    min-width: 600px;
     margin: 0 auto;
     border-bottom: solid 1px black;
   }
