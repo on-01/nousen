aa93a620f26d2988900f0b604438febc37b989c4
github m-omiya <m.omiya1127@gmail.com>
2025-02-03 16:55:40 +0900

    DELETE:文言削除

--- src/components/licensePage/WorkshopKenchiku.vue
+++ src/components/licensePage/WorkshopKenchiku.vue
@@ -108,11 +108,11 @@
     <p class="workshop-bottom">
       ※仕事や体調不良などで欠席した場合はオンライン講座に無料振替ができます
     </p>
-    <p class="workshop-bottom -leftText">
+    <!-- <p class="workshop-bottom -leftText">
       <span>【締め切りについて】</span>
       <br />
       お席に空きがあれば講習日前日まで受付しております。お問い合わせください。
-    </p>
+    </p> -->
 
     <div class="workshop-orangeLine">
       <div class="workshop-orangeLine__inner">
