bbae2714732b0577dac06cbe3dee32b15621bfce
github m-omiya <m.omiya1127@gmail.com>
2025-04-07 16:16:29 +0900

    ADD：ヘッダーSP項目追加

--- src/components/TheMainHeader.vue
+++ src/components/TheMainHeader.vue
@@ -205,6 +205,11 @@
             </g-link>
           </div>
           <div class="pagelink-title">
+            <g-link to="https://nousen.blog/oshiharaihouhou/">
+              <p>お支払い方法</p>
+            </g-link>
+          </div>
+          <div class="pagelink-title">
             <g-link to="/guide">
               <p>特定商取引法に基づく表記</p>
             </g-link>
@@ -212,6 +217,16 @@
           <div class="pagelink-title">
             <g-link to="/privacy">
               <p>プライバシーポリシー</p>
+            </g-link>
+          </div>
+          <div class="pagelink-title">
+            <g-link to="/anti-social-force">
+              <p>反社会的勢力排除宣言</p>
+            </g-link>
+          </div>
+          <div class="pagelink-title">
+            <g-link to="https://nousen.blog/koushiuketuke/">
+              <p>講師募集について</p>
             </g-link>
           </div>
         </section>
