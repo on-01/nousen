8838548ce46acd79d0df0ccea2f167f612f8f698
github m-omiya <m.omiya1127@gmail.com>
2025-04-14 12:57:48 +0900

    ADD：調製

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -10,19 +10,11 @@
   </Layout>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
-}
-</static-query>
-
 <script>
   export default {
     data() {
       return {
-        targetDate: new Date("2025-06-01"), // 目標日
+        targetDate: new Date("2025-06-01"),
         today: new Date(),
       };
     },
@@ -33,12 +25,13 @@ query {
         return diffDays >= 0 ? diffDays : 0;
       },
       targetDateFormatted() {
-        const options = { month: "long", day: "numeric" };
-        return this.targetDate.toLocaleDateString("ja-JP", options);
+        const year = this.targetDate.getFullYear();
+        const month = this.targetDate.getMonth() + 1;
+        const day = this.targetDate.getDate();
+        return `${year}年${month}月${day}日`;
       },
     },
     mounted() {
-      // 日付を毎時更新
       setInterval(() => {
         this.today = new Date();
       }, 1000 * 60 * 60);
