b5458c1753103f43731982b8850540a5f5ee034b
github m-omiya <m.omiya1127@gmail.com>
2025-04-15 13:07:23 +0900

    DELETE:タグの削除

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -2,8 +2,16 @@
   <Layout>
     <div class="test">
       <p>
-        {{ targetDateFormatted }}まで あと
-        <span>{{ remainingDays }}</span>
+        {{ targetDateFormatted1 }}まで あと
+        <span>{{ remainingDays1 }}</span>
+        日
+      </p>
+    </div>
+
+    <div class="test2">
+      <p>
+        {{ targetDateFormatted2 }}まで あと
+        <span>{{ remainingDays2 }}</span>
         日
       </p>
     </div>
@@ -14,20 +22,32 @@
   export default {
     data() {
       return {
-        targetDate: new Date("2025-06-01"),
+        targetDate1: new Date("2025-06-01"),
+        targetDate2: new Date("2025-07-01"),
         today: new Date(),
       };
     },
     computed: {
-      remainingDays() {
-        const diffTime = this.targetDate - this.today;
+      remainingDays1() {
+        const diffTime = this.targetDate1 - this.today;
         const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
         return diffDays >= 0 ? diffDays : 0;
       },
-      targetDateFormatted() {
-        const year = this.targetDate.getFullYear();
-        const month = this.targetDate.getMonth() + 1;
-        const day = this.targetDate.getDate();
+      remainingDays2() {
+        const diffTime = this.targetDate2 - this.today;
+        const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return diffDays >= 0 ? diffDays : 0;
+      },
+      targetDateFormatted1() {
+        const year = this.targetDate1.getFullYear();
+        const month = this.targetDate1.getMonth() + 1;
+        const day = this.targetDate1.getDate();
+        return `${year}年${month}月${day}日`;
+      },
+      targetDateFormatted2() {
+        const year = this.targetDate2.getFullYear();
+        const month = this.targetDate2.getMonth() + 1;
+        const day = this.targetDate2.getDate();
         return `${year}年${month}月${day}日`;
       },
     },
@@ -50,7 +70,8 @@
 </script>
 
 <style lang="scss" scoped>
-  .test {
+  .test,
+  .test2 {
     width: 100%;
     font-size: 1.2rem;
     text-align: center;
