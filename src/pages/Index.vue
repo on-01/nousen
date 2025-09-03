f02a3dc3ed3f32d8942efeca521383feb0b5bbb5
github m-omiya <m.omiya1127@gmail.com>
2025-09-03 11:07:54 +0900

    ADD:修正１

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,9 +3,9 @@
     <div id="js-scroll_hidden" class="fixed-note" v-show="isShow">
       <div class="fixed-note-inner">
         <p class="fixed-note-text">
-          🔔 電験三種CBT試験スタート！
+          2級土木施工管理技士【二次試験対策】講習会 ✍️
           <br />
-          受験される皆さま、最後まであきらめずに頑張ってください！
+          9/14(日) 東京会場【受付中】
         </p>
         <img
           class="fixed-note-icon"
