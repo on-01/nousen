8ecb62c77558b73f075a7ce39e2c28c3f783f4fc
github m-omiya <m.omiya1127@gmail.com>
2025-09-22 16:16:26 +0900

    ADD:調整２

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -178,7 +178,11 @@
         <!-- <div class="scroll_top" id="js-scroll_top">
           <img src="../assets/img/front/scroll.svg" alt="scroll" />
         </div> -->
-        <img src="../assets/img/front/section01/top-map.png" alt="" />
+        <img
+          class="top-map"
+          src="../assets/img/front/section01/top-map.png"
+          alt=""
+        />
 
         <div class="more_qualification">
           <!-- <g-link to="/licenses">
@@ -1557,6 +1561,13 @@ query {
       margin-left: 10px;
       vertical-align: text-bottom;
     }
+  }
+
+  .top-map {
+    display: block;
+    margin: 0 auto;
+    max-width: 1024px;
+    width: 90%;
   }
 
   .section02 {
