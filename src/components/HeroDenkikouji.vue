07774c0fc5cd387207d2d8a758f959ef91e622e6
github m-omiya <m.omiya1127@gmail.com>
2025-10-30 16:42:29 +0900

    ADD:調整6

--- src/components/HeroDenkikouji.vue
+++ src/components/HeroDenkikouji.vue
@@ -28,9 +28,9 @@
       <p class="sub-title">
         <!-- <a href="https://denken.nouryoku.com/kiso-kouza">基礎講座</a>
         | -->
-        <a href="https://nousen.blog/tugaku-d/">通学講座</a>
+        <a>通学講座</a>
         |
-        <a href="https://doboku-online.nouryoku.com/">オンライン講座</a>
+        <a>オンライン講座</a>
         <!-- |
         <a href="https://t.kouza.nouryoku.com/">通信講座</a> -->
       </p>
