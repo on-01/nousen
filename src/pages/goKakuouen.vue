b26b961bf4af277a725215aa1ada0cf44b74f0a6
github m-omiya <m.omiya1127@gmail.com>
2025-11-20 09:51:10 +0900

    ADD:修正１

---
+++ src/pages/goKakuouen.vue
@@ -0,0 +1,309 @@
+<template>
+  <Layout>
+    <div class="goKakuouen">
+      <h2>🎓 GO!資格 合格応援制度</h2>
+      <img
+        class="goKakuouen-top-img"
+        src="../assets/img/gokakuouen/gokakuouen_top.jpg"
+        alt="GO!資格 合格応援制度"
+      />
+      <p class="goKakuouen-text">
+        能センでは、受講生の皆さまが試験に合格し、資格を取得できるよう、心強いサポートとして「合格応援割引制度」をご用意しています。
+        <br />
+        <br />
+        この割引制度は、これから学習を始める皆さまの経済的なご負担を少しでも軽減し、学習に専念していただけるようサポートすることを目的としています。
+        <br />
+        費用面での不安を解消し、安心して講座に取り組んでいただけるよう、能センは全力で応援しています！
+        <br />
+        <br />
+        各割引プランの詳細については、ぜひ以下をご覧ください。あなたにぴったりのプランがきっと見つかります！
+      </p>
+      <h3 class="goKakuouen-blue">🎓 GO!資格 合格応援制度</h3>
+      <div class="goKakuouen-wrapper">
+        <p class="goKakuouen-title">再受講割引</p>
+        <div class="goKakuouen-flex">
+          <img
+            src="../assets/img/gokakuouen/gokakuouen_img1.jpg"
+            alt="GO!資格 合格応援制度"
+          />
+          <div class="goKakuouen-content">
+            <p class="goKakuouen-heading1">再受講割引でさらにお得！</p>
+            <p class="goKakuouen-heading2">
+              再受講割引 各5000円引き（科目数×5000円）
+            </p>
+            <p class="goKakuouen-text">
+              同じ科目・コースの再受講をする場合、各科目につき5000円引きになります。
+              <br />
+              例.第三種電気主任技術者 ４科目セットが再受講の場合
+              <br />
+              4科目×5000円 ＝
+              <span class="goKakuouen-text-line">20000円引き</span>
+              <br />
+              <br />
+              ※割引の併用はできません（キャンペーンコードは別）
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="goKakuouen-wrapper">
+        <p class="goKakuouen-title">若チャレ割</p>
+        <div class="goKakuouen-flex">
+          <img
+            src="../assets/img/gokakuouen/gokakuouen_img2.jpg"
+            alt="GO!資格 合格応援制度"
+          />
+          <div class="goKakuouen-content">
+            <p class="goKakuouen-heading1">
+              若者の国家資格取得を全力サポート！講習料金割引制度！
+            </p>
+            <p class="goKakuouen-heading2">若者チャレンジ応援割 5000円引き</p>
+            <p class="goKakuouen-text">
+              この制度は、
+              <span class="goKakuouen-text-bold">20歳</span>
+              以下の若年層が国家資格の勉強に取り組みやすくなるよう、講座料金の負担を軽減することを目的としています。未来に向けてスキルを磨きたい若者を応援するための特別な制度です。
+              <br />
+              <br />
+              ※割引の併用はできません（キャンペーンコードは別）
+              <br />
+              ※受付時に年齢を確認させていただきます
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="goKakuouen-wrapper">
+        <p class="goKakuouen-title">女子応援割</p>
+        <div class="goKakuouen-flex">
+          <img
+            src="../assets/img/gokakuouen/gokakuouen_img3.jpg"
+            alt="GO!資格 合格応援制度"
+          />
+          <div class="goKakuouen-content">
+            <p class="goKakuouen-heading1">
+              女性のキャリアを応援！資格取得やスキルアップを目指す方に割引制度をご用意
+            </p>
+            <p class="goKakuouen-heading2">女子応援割 各2000円引き</p>
+            <p class="goKakuouen-text">
+              この制度は、女性が資格取得やスキルアップに挑戦する際、講座料金が割引される魅力的な仕組みです。女性のキャリア形成をサポートし、新たな分野への挑戦やさらなる成長を後押しすることを目的としています。「次のステージに進みたい！」そんな思いを、ぜひこの制度で叶えてみませんか？
+              <br />
+              <br />
+              ※割引の併用はできません（キャンペーンコードは別）
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="goKakuouen-wrapper">
+        <p class="goKakuouen-title">シニア応援割</p>
+        <div class="goKakuouen-flex">
+          <img
+            src="../assets/img/gokakuouen/gokakuouen_img4.jpg"
+            alt="GO!資格 合格応援制度"
+          />
+          <div class="goKakuouen-content">
+            <p class="goKakuouen-heading1">
+              60歳以上のシニア向け！資格取得やスキルアップを応援する割引制度
+            </p>
+            <p class="goKakuouen-heading2">シニア応援割 各2000円引き</p>
+            <p class="goKakuouen-text">
+              この制度は、
+              <span class="goKakuouen-text-bold">60歳以上</span>
+              の方が資格取得やスキルアップを目指す際に、講座料金が割引されるお得な仕組みです。シニア世代のキャリア形成をサポートし、新たな分野への挑戦やスキル向上を後押しすることを目的としています。「もう遅い」と思わず、これを機会に一歩踏み出してみませんか？
+              <br />
+              <br />
+              ※割引の併用はできません（キャンペーンコードは別）
+              <br />
+              ※受付時に年齢を確認させていただきます
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="goKakuouen-wrapper">
+        <p class="goKakuouen-title">団体割・超団体割</p>
+        <div class="goKakuouen-flex">
+          <img
+            src="../assets/img/gokakuouen/gokakuouen_img5.jpg"
+            alt="GO!資格 合格応援制度"
+          />
+          <div class="goKakuouen-content">
+            <p class="goKakuouen-heading1">
+              仲間と一緒にお得に！団体割引でさらに安く
+            </p>
+            <p class="goKakuouen-heading2">
+              団体割 （5名以上）全員10%OFF
+              <br />
+              超団体割（10名以上）全員15%OFF
+            </p>
+            <p class="goKakuouen-text">
+              ※割引の併用はできません（キャンペーンコードは別）
+              <br />
+              ※団体割は新規のお申込みが対象になります
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <!-- LINE -->
+      <p class="goKakuouen-friend">LINEお友達クーポン</p>
+      <a href="https://page.line.me/947ygtge?openQrModal=true" target="_blank">
+        <img
+          class="goKakuouen-line-img"
+          src="../assets/img/gokakuouen/gokakuouen_img6.png"
+          alt="GO!資格 合格応援制度"
+        />
+      </a>
+      <p class="goKakuouen-heading1">
+        LINE公式アカウント開設！お友達登録キャンペーン実施中
+      </p>
+      <p class="goKakuouen-text">
+        ただいま、
+        <span class="goKakuouen-text-bold">LINE公式アカウント</span>
+        の開設を記念して、全講座で使える
+        <span class="goKakuouen-text-bold">500円割引キャンペーンコード</span>
+        をプレゼント！
+        <br />
+        期間限定の特別キャンペーンですので、ぜひお早めにご利用ください。
+        <br />
+        ※他の割引と併用可能です。
+      </p>
+      <!-- 建設業 創業支援制度 -->
+      <h3 class="goKakuouen-orange">建設業 創業支援制度</h3>
+      <img
+        class="goKakuouen-startup-img"
+        src="../assets/img/gokakuouen/gokakuouen_img7.jpg"
+        alt="建設業 創業支援制度"
+      />
+      <p class="goKakuouen-text">
+        能センでは建設業発展のため、創業支援として
+        <br />
+        創業２年以内の個人事業主、及び法人企業従業員様全ての方を対象に、
+        <br />
+        受講料を２０％割引でご提供いたします。
+        <br />
+        特に提出書類はございませんが、本社住所・代表電話番号・代表者氏名が必要になります。
+        <br />
+        <br />
+        <span class="goKakuouen-text-line">
+          能センは建設業の発展に微力ながら尽力させていただきます！
+        </span>
+        <br />
+        <br />
+        ※創業日から数えて２年以内の企業が対象になります
+        ※他の割引とは併用できません
+      </p>
+      <!-- 資格で未来を切り開く。能センは挑戦を応援します！ -->
+      <h3 class="goKakuouen-blue">
+        資格で未来を切り開く。能センは挑戦を応援します！
+      </h3>
+      <div class="goKakuouen-flex">
+        <div class="goKakuouen-content">
+          <img
+            src="../assets/img/gokakuouen/gokakuouen_img8.png"
+            alt="資格で未来を切り開く。能センは挑戦を応援します！"
+          />
+          <p class="goKakuouen-text">
+            能センでは全てのプリント・資料にFSC認証を受けたエコペーパーを使用しております。FSC認証とは、持続可能な森林活用・保全を目的として誕生した、「適切な森林管理」を認証する国際的な制度です。
+            <br />
+            FSC認証とは
+            <a
+              href="https://shitte-erabo.net/aboutfsc/certification/"
+              target="_blank"
+            >
+              https://shitte-erabo.net/aboutfsc/certification/
+            </a>
+          </p>
+        </div>
+        <div class="goKakuouen-content">
+          <img
+            src="../assets/img/gokakuouen/gokakuouen_img9.jpg"
+            alt="資格で未来を切り開く。能センは挑戦を応援します！"
+          />
+          <p class="goKakuouen-text">
+            建設業の未来を応援！！
+            <br />
+            子供たちが夢と希望を持って働ける世の中へ！
+            <br />
+            能センでは受講生１名につき１００円を「こどもの未来応援基金」へ寄付をいたします。
+            <br />
+            <a href="https://kodomohinkon.go.jp/" target="_blank">
+              こどもの未来応援基金
+            </a>
+            は、こどもの貧困対策を進めるための官民連携プロジェクトのひとつとして、２０１５年に創設されました。
+          </p>
+        </div>
+      </div>
+      <g-link to="/">
+        <img
+          class="goKakuouen-nousen-img"
+          src="../assets/img/common/main_logo.svg"
+          alt="資格で未来を切り開く。能センは挑戦を応援します！"
+        />
+      </g-link>
+    </div>
+  </Layout>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+    siteUrl
+		siteDescription
+  }
+}
+</static-query>
+
+<script>
+  import Hero from "@/components/Hero.vue";
+  export default {
+    metaInfo() {
+      return {
+        titleTemplate: "GO!資格合格応援制度｜能セン-合格への近道-",
+        meta: [
+          {
+            name: "description",
+            content:
+              "電気、建築、設備関連を中心とした受験対策講習会を開催しています能センのページです。GO!資格合格応援制度について記載しています。",
+          },
+          {
+            key: `og:title`,
+            property: `og:title`,
+            content: `GO!資格合格応援制度｜能セン-合格への近道-`,
+            // content: `プライバシーポリシー | ${this.$static.metadata.siteName}`,
+          },
+          {
+            key: `og:url`,
+            property: `og:url`,
+            content: `${this.$static.metadata.siteUrl}/goKakuouen`,
+          },
+        ],
+      };
+    },
+    components: {
+      Hero,
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .goKakuouen {
+    font-family: "メイリオ", sans-serif;
+    background-color: #ffffff;
+    padding: 76px 30px;
+    max-width: 860px;
+    margin: 0 auto;
+
+    h2 {
+      font-size: 26px;
+      margin: 0 0 16px;
+      font-weight: 700;
+    }
+
+    .goKakuouen-top-img {
+      width: 100%;
+      height: auto;
+    }
+
+    .goKakuouen-text {
+      margin: 0 0 16px;
+    }
+  }
+</style>
