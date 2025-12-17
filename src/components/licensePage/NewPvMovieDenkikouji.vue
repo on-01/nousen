ee9b8624c59a182971db02931db5aa1f17b7fcab
github m-omiya <m.omiya1127@gmail.com>
2025-12-17 14:17:12 +0900

    ADD：調整

--- src/components/licensePage/NewPvMovieDenkikouji.vue
+++ src/components/licensePage/NewPvMovieDenkikouji.vue
@@ -8,7 +8,7 @@
           <iframe
             width="560"
             height="315"
-            src="https://youtu.be/GlbmItQmSZM?si=gPK8Y3JWHLhlVHSQ"
+            src="https://www.youtube.com/embed/GlbmItQmSZM"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
