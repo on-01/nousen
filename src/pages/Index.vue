0ba84ff0d0026f0870eb8a3832be8d00d13b57ad
github m-omiya <m.omiya1127@gmail.com>
2026-02-17 12:17:02 +0900

    ADD：調整

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1695,11 +1695,10 @@ query {
   .top-map {
     display: block;
     margin: 80px auto 0 auto;
-    max-width: 1024px;
-    width: 80%;
+    max-width: 924px;
+    width: 90%;
     @media print, screen and (max-width: 1000px) {
       margin-top: 20px;
-      width: 90%;
     }
   }
 
