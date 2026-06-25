a2c537c95784ae207af85206510228faa9ef152f
github m-omiya <m.omiya1127@gmail.com>
2026-06-25 12:34:39 +0900

    ADD:画像差し替え

--- src/components/licensePage/WorkshopDenkikouji.vue
+++ src/components/licensePage/WorkshopDenkikouji.vue
@@ -3,11 +3,11 @@
     <p class="workshop-title">２級電気工事施工管理技士 通学講座(講習会)</p>
     <div class="workshop-topImg">
       <img
-        src="../../assets/img/license_page/workshop-img1.webp"
+        src="../../assets/img/license_page/workshop-img1-1.webp"
         alt="2級電気工事施工管理技士 講習会 通信講座 web講座"
       />
       <img
-        src="../../assets/img/license_page/workshop-img2.webp"
+        src="../../assets/img/license_page/workshop-img2-2.webp"
         alt="2級電気工事施工管理技士 講習会 通信講座 web講座"
       />
     </div>
