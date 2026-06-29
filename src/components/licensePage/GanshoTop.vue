14457e136bd275dac0d9844f1bdf9b4aea4a1769
github m-omiya <m.omiya1127@gmail.com>
2026-06-29 17:26:58 +0900

    ADD:調整２

--- src/components/licensePage/GanshoTop.vue
+++ src/components/licensePage/GanshoTop.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="gansho">
-    <div class="gansho-contents">
-      <p class="gansho-title">願書取り寄せ・記入サポート</p>
-      <p class="gansho-text">
-        書面申請が必要な受講生には、願書の取り寄せ・記入方法をサポートします。
-      </p>
-      <p class="bottom">※一部の申請区分が対象です。</p>
+    <div class="gansho-wrapper">
+      <div class="gansho-contents">
+        <p class="gansho-title">願書取り寄せ・記入サポート</p>
+        <p class="gansho-text">
+          書面申請が必要な受講生には、願書の取り寄せ・記入方法をサポートします。
+        </p>
+        <p class="gansho-bottom">※一部の申請区分が対象です。</p>
+      </div>
+      <img src="../../assets/img/license_page/gansho-img.webp" alt="" />
     </div>
-    <img src="../../assets/img/license_page/gansho-img.webp" alt="" />
   </div>
 </template>
 <p></p>
@@ -21,16 +23,44 @@
 
 <style lang="scss" scoped>
   .gansho {
-    max-width: 768px;
+    max-width: 450px;
     margin: 0 auto;
     z-index: 0;
     position: relative;
     font-family: "メイリオ", sans-serif;
     background-color: #fff7d5;
+    display: flex;
+  }
+
+  .gansho-wrapper {
+    background-color: #ffffff;
+    padding: 24px;
   }
 
   .gansho-contents {
-    display: flex;
-    justify-content: space-between;
+    img {
+      display: block;
+    }
+    .gansho-title {
+      position: relative;
+      margin: 0;
+      color: #1262b2;
+
+      ::after {
+        position: absolute;
+        content: "";
+        width: 100px;
+        height: 2px;
+        background: #399fec;
+      }
+    }
+
+    .gansho-text {
+      margin: 0;
+    }
+
+    .gansho-bottom {
+      margin: 0;
+    }
   }
 </style>
